1740d1872e052ff4e473ff8c9e1f7c7bab0969b9
Arun <82587103+98arun@users.noreply.github.com>
2021-08-13 18:38:20 +0530

    netflix

---
+++ Assets/Js/index.js
@@ -0,0 +1,6 @@
+let more = document.getElementById("more");
+more.addEventListener("click", () => {
+  let hide = document.getElementById("hide");
+  hide.style.visibility = "visible";
+  more.style.visibility = "hidden";
+});
