fc7d9f7b9069e84322c196782f5b99ea6414d51e
Arun <82587103+98arun@users.noreply.github.com>
2021-08-16 18:36:20 +0530

    final

--- Assets/Js/index.js
+++ Assets/Js/index.js
@@ -1,6 +1,24 @@
+//signin page code
 let more = document.getElementById("more");
-more.addEventListener("click", () => {
-  let hide = document.getElementById("hide");
-  hide.style.visibility = "visible";
-  more.style.visibility = "hidden";
-});
+if (more) {
+  more.addEventListener("click", () => {
+    let hide = document.getElementById("hide");
+    hide.style.visibility = "visible";
+    more.style.visibility = "hidden";
+  });
+}
+
+//home-page toggle accordian code
+const accordian = document.querySelectorAll(".accordian-item");
+if (accordian) {
+  accordian.forEach((faq) => {
+    faq.addEventListener("click", () => {
+      console.log("click");
+      if (faq) {
+        faq.classList.toggle("active");
+        // faq.classList.add("active");
+        // faq.classList.remove("active");
+      }
+    });
+  });
+}
